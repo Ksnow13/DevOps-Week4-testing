2580231cad9b016d6e7ec2d5c6f6f56f5b09a955
Ksnow <107448069+Ksnow13@users.noreply.github.com>
2023-05-23 10:34:26 -0230

    Added new test to demo GH actions.

--- src/test/java/com/keyin/domain/CourseAdminTest.java
+++ src/test/java/com/keyin/domain/CourseAdminTest.java
@@ -31,7 +31,34 @@ public class CourseAdminTest {
         courseToFind.setName("Java");
 
         Assertions.assertTrue(courseAdmin.getCourseListByStudent(studentToFind).contains(courseToFind));
-
     }
+
+    @Test
+    @DisplayName("Failing Test Get List By Student")
+    public void testGetCourseListByStudent_Failing(){
+
+        CourseAdmin courseAdmin = new CourseAdmin();
+
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Jamie");
+
+        Course course = new Course();
+        course.setId(1);
+        course.setName("Java");
+
+        courseAdmin.addCourseToStudent(student, course);
+
+        Student studentToFind = new Student();
+        studentToFind.setId(1);
+        studentToFind.setName("Jamie");
+
+        Course courseToFind = new Course();
+        courseToFind.setId(1);
+        courseToFind.setName("JavaScript");
+
+        Assertions.assertTrue(courseAdmin.getCourseListByStudent(studentToFind).contains(courseToFind));
+    }
+
 
 }
